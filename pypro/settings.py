1432837717703447b63119a16be892588188a6c4
romilsoncordeiro <romilsonlemes@gmail.com>
2021-01-12 01:53:57 -0300

    Finalização da Configuração da Otimização de Uploads com Collectfast
    close #57

--- pypro/settings.py
+++ pypro/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'collectfast',
     'django.contrib.staticfiles',
     'pypro.base',
 ]
@@ -130,22 +131,35 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
+COLLECTFAST_ENABLED = False
+
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
+
 # STORAGE CONFIGURATION IN S3 AWS
-if AWS_ACCESS_KEY_ID:
+# -----------------------------------------------------------------
+"""
+# Observação de código: Na linha abaixo tem um parametro dopois do IF assim ( # pragma: no cover )
+# Este parametro serve para fazer com que o CODCOV desconsidere a validação de cobertura de código
+# Sem este parametro, o CODCOV vai ficar invalidando o código e não vai deixar passar o deploy.
+# -------------------------------------------------------------------------------------------------
+"""
+if AWS_ACCESS_KEY_ID:  # pragma: no cover
     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }
     AWS_PRELOAD_METADATA = True
     AWS_AUTO_CREATE_BUCKET = False
     AWS_QUERYSTRING_AUTH = True
-    AWS_S3_CUSTOM_DOMAIN = None
-    AWS_DEFAULT_ACL = 'private'
+    AWS_S3_CUSTOM_DOMAIN = False
+
+    COLLECTFAST_ENABLED = True
+
+    AWS_DEFAULT_ACL = None
 
     # Static Assets
     # --------------------------------------------------------
-    STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
+    STATIC_FILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
     STATIC_S3_PATH = 'static'
     STATIC_ROOT = f'/{STATIC_S3_PATH}/'
     STATIC_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{STATIC_S3_PATH}/'
