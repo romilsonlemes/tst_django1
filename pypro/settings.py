e600cd7033ed8d58dc838e101312da60b7008abc
romilsoncordeiro <romilsonlemes@gmail.com>
2021-02-17 20:57:21 -0300

    Foi configurado o Rodapé do Projeto.
    close #145

--- pypro/settings.py
+++ pypro/settings.py
@@ -156,6 +156,7 @@ AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 # -------------------------------------------------------------------------------------------------
 """
 if AWS_ACCESS_KEY_ID:  # pragma: no cover
+    COLLECTFAST_ENABLED = True
     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }
@@ -163,8 +164,6 @@ if AWS_ACCESS_KEY_ID:  # pragma: no cover
     AWS_AUTO_CREATE_BUCKET = False
     AWS_QUERYSTRING_AUTH = True
     AWS_S3_CUSTOM_DOMAIN = False
-
-    COLLECTFAST_ENABLED = True
 
     AWS_DEFAULT_ACL = 'private'
 
