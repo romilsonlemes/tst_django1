a048d944603367b03cf88e8aa552fb53622481be
romilsoncordeiro <romilsonlemes@gmail.com>
2021-02-24 02:22:38 -0300

    Foi implementado o Contexto de Template Esta implementação permite ao Django Receber dados através do Contexto do Python.
    1.0 - Falhas Encontradas
    1.1 - Retirado o # - Comentário do Views.py
    
    close #179

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -4,8 +4,3 @@ from django.shortcuts import render
 def video(request, slug):  # pragma: no cover
     video = {'titulo': 'Video Aperitivos: Motivação', 'vimeo_id': '515479903'}
     return render(request, 'aperitivos/video.html', context={'video': video})
-
-
-#Video da jessica: 515125492
-#Video da Disney.: 515479903
-
