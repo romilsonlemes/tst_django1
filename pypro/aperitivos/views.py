216b3718242833ce2287c6255b894ec974e66909
romilsoncordeiro <romilsonlemes@gmail.com>
2021-02-24 15:44:25 -0300

    Foi implementado o desenvolvimento de Video de Acordo com Slug.
    Esta implementação aborda a passagem de parametros na URL do site para que o video seja trocado dependendo do parametro que é passado no portal para aparecer outro vídeo sem precisar de banco de dados para isso.
    
    close #181

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):  # pragma: no cover
-    video = {'titulo': 'Video Aperitivos: Motivação', 'vimeo_id': '515479903'}
+    videos = {
+        'motivacao': {'titulo': 'Video Aperitivos: Motivação', 'vimeo_id': '515479903'},
+        'jessica-gata': {'titulo': 'Jessica e a sua gatinha', 'vimeo_id': '515125492'},
+    }
+    video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
