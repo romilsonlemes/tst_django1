778ed57a593af881240826ffd4982bff0080e78a
romilsoncordeiro <romilsonlemes@gmail.com>
2021-03-04 17:08:36 -0300

    Foram executados os  Testes Com e Sem Migrações.
    Dois arquivos foram renomedos os seus nomes devido a execuçção de todos os testes.
    
    Foi alterado os nomes que iniciavam por tests_indices.py  para test_indice.py e o arquivo tests_video.py para test_video.py.
    
    Esta alteração foi realizada por que quando é executado um teste geral, o pytest procura todos os arquivos que estao na pasta do projeto que iniciam com test_xxxx por isso estes arquivos foram renomeados o inicio de seus nomes.
    
    * Foi resolvido a falha do pytest incluindo um registro na base de dados para realizar o teste necessário.
    
    * -> Finalizado os testes com a Criação Automágica de Propriedades de Modelo
    
    Foi feito o merge do pipfile.lock
    
    close #208

--- pypro/aperitivos/tests/test_indice.py
+++ pypro/aperitivos/tests/test_indice.py
@@ -1,10 +1,18 @@
 import pytest
 from django.urls import reverse
+from model_mommy import mommy
+
+from pypro.aperitivos.models import Video
 from pypro.django_assertions import assert_contains
 
 
 @pytest.fixture
-def resp(client):  # pragma: no cover
+def videos(db):  # pragma: no cover
+    return mommy.make(Video, 3)
+
+
+@pytest.fixture
+def resp(client, videos):  # pragma: no cover
     return client.get(reverse('aperitivos:indice'))
 
 
@@ -12,24 +20,12 @@ def test_status_code(resp):  # pragma: no cover
     assert resp.status_code == 200
 
 
-@pytest.mark.parametrize(
-    'titulo',
-    [
-        'Video Aperitivos: Motivação',
-        'Jessica e a sua gatinha'
-    ]
-)
-def test_titulo_video(resp, titulo):  # pragma: no cover
-    assert_contains(resp, titulo)
+def test_titulo_video(resp, videos):  # pragma: no cover
+    for video in videos:
+        assert_contains(resp, video.titulo)
 
 
-@pytest.mark.parametrize(
-    'slug',
-    [
-        'motivacao',
-        'jessica-gata'
-    ]
-)
-def test_link_video(resp, slug):  # pragma: no cover
-    video_link = reverse('aperitivos:video', args=(slug,))
-    assert_contains(resp, f'href="{video_link}"')
+def test_link_video(resp, videos):  # pragma: no cover
+    for video in videos:
+        video_link = reverse('aperitivos:video', args=(video.slug,))
+        assert_contains(resp, f'href="{video_link}"')
--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,16 +2,9 @@ from django.shortcuts import render, get_object_or_404
 
 from pypro.aperitivos.models import Video
 
-videos = [
-    Video(slug='motivacao', titulo='Video Aperitivos: Motivação', vimeo_id='515479903'),
-    Video(slug='jessica-gata', titulo='Jessica e a sua gatinha', vimeo_id='515125492'),
-]
-
-
-videos_dct = {v.slug: v for v in videos}
-
 
 def indice(request):  # pragma: no cover
+    videos = Video.objects.order_by('creation').all()
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
 
